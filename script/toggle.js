96d14d1a102f86e4d45abf7222523a424193e6e9
topgreens <topgreens@mail.com>
2019-07-12 23:51:35 -0500

    mobile approach updated

---
+++ script/toggle.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  /* Close/hide the sidenav */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
